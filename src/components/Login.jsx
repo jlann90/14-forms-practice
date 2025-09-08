b2aadacd666cfa59cf368d42ccbaf5f49c7b08df
James Lannon <jameslannon@Jamess-MacBook-Pro.local>
2025-09-08 14:09:14 -0700

    feat: update Login to get user entered email and password using enteredValues state, and handleInputChange event handlers on inputs, update form with onSumbit handleSubmit handler and even.preventDefault to prevent the form sending an http request to the server

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,17 +1,53 @@
+import { useState } from "react";
+
 export default function Login() {
+  const [enteredValues, setEnteredValues] = useState({
+    email: "",
+    password: "",
+  });
+
+  function handleSubmit(event) {
+    // this prevents the browsers default behavior of sending a form http request to the server (causing the project to reload) in React apps
+    event.preventDefault();
+    console.log("Submitted");
+    console.log("Email: " + enteredValues.email);
+    console.log("Password: " + enteredValues.password);
+  }
+
+  function handleInputChange(identifier, value) {
+    setEnteredValues((prevValues) => ({
+      ...prevValues,
+      [identifier]: value,
+    }));
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Login</h2>
 
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input
+            value={enteredValues.email}
+            onChange={(event) => handleInputChange("email", event.target.value)}
+            id="email"
+            type="email"
+            name="email"
+          />
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input
+            value={enteredValues.password}
+            onChange={(event) =>
+              handleInputChange("password", event.target.value)
+            }
+            id="password"
+            type="password"
+            name="password"
+          />
         </div>
       </div>
 
